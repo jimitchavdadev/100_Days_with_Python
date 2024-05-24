ddc4148743f514b598133948a4aa871c51dff42f
jimitchavdadev <gem.programming@protonmail.com>
2024-05-24 13:32:31 +0530

    Day-1

---
+++ Day_1/Band_Name_Generator.py
@@ -0,0 +1,6 @@
+# Band Name Generator
+
+print("welcome to the band name Generator.")
+city_name=input("whats the name of the city you grew up in?\n")
+pet_name=input("whats your pet's name?\n")
+print(f"your band name can be {city_name} {pet_name}")
