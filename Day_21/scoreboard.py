c187582fb66f032a88e0b28ccea080772fb48ec5
Jimit Chavda <jimitchavdadev@gmail.com>
2024-06-05 19:04:39 +0530

    Day-21

---
+++ Day_21/scoreboard.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.hideturtle()
+        self.penup()
+        self.goto(0, 260)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()  # Clear previous score
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER!", align="center", font="Arial",)
+
