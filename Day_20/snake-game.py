2bb15a252b4b0daf0b3f468f84ddd1af9bd007b0
Jimit Chavda <jimitchavdadev@gmail.com>
2024-06-15 13:32:53 +0530

    Day-20

--- Day_20/snake-game.py
+++ Day_20/snake-game.py
@@ -1,28 +1,28 @@
-from turtle import Turtle, Screen
-from snake import Snake
-import time
+from turtle import Turtle, Screen  # Import necessary classes from the turtle module
+from snake import Snake  # Import the Snake class from your snake.py file
+import time  # Import the time module
 
+# Set up the screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-screen.tracer(0)
+screen.tracer(0)  # Turn off animation initially
 
-snake = Snake()
+snake = Snake()  # Create an instance of the Snake class
 
+# Listen for key events and call the corresponding methods in the Snake class
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-
-game_is_on = True
+game_is_on = True  # Flag to control the game loop
 while game_is_on:
-    screen.update()
-    time.sleep(0.1)
+    screen.update()  # Update the screen
+    time.sleep(0.1)  # Pause to control the speed of the game
 
-    snake.move()
+    snake.move()  # Move the snake
 
-
-screen.exitonclick()
+screen.exitonclick()  # Exit the game when the screen is clicked
--- Day_20/snake.py
+++ Day_20/snake.py
@@ -1,5 +1,6 @@
-from turtle import Turtle
+from turtle import Turtle  # Import the Turtle class from the turtle module
 
+# Constants for starting positions, move distance, and directions
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 
@@ -9,40 +10,44 @@ LEFT = 180
 RIGHT = 0
 
 class Snake:
-
     def __init__(self):
-        self.segments = []
-        self.create_snake()
-        self.head = self.segments[0]
+        self.segments = []  # List to store the snake's segments
+        self.create_snake()  # Method to create the initial snake
+        self.head = self.segments[0]  # Head of the snake
 
     def create_snake(self):
+        """Create the initial snake with three segments."""
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            new_segment = Turtle("square")  # Create a new segment as a square Turtle
+            new_segment.color("white")  # Set the color of the segment
+            new_segment.penup()  # Lift the pen to avoid drawing lines between segments
+            new_segment.goto(position)  # Move the segment to the specified position
+            self.segments.append(new_segment)  # Add the segment to the snake's segments list
 
     def move(self):
+        """Move the snake forward by MOVE_DISTANCE."""
         for seg_num in range(len(self.segments) - 1, 0, -1):
-            new_x = self.segments[seg_num - 1].xcor()
-            new_y = self.segments[seg_num - 1].ycor()
-            self.segments[seg_num].goto(new_x, new_y)
-        self.head.forward(MOVE_DISTANCE)
+            new_x = self.segments[seg_num - 1].xcor()  # Get the x-coordinate of the previous segment
+            new_y = self.segments[seg_num - 1].ycor()  # Get the y-coordinate of the previous segment
+            self.segments[seg_num].goto(new_x, new_y)  # Move the current segment to the new position
+        self.head.forward(MOVE_DISTANCE)  # Move the head of the snake forward by MOVE_DISTANCE
 
     def up(self):
-        if self.head.heading() != DOWN:
-            self.head.setheading(UP)
+        """Change the snake's direction to up if not currently moving down."""
+        if self.head.heading() != DOWN:  # If the snake is not currently moving down
+            self.head.setheading(UP)  # Change the snake's direction to up
 
     def down(self):
-        if self.head.heading() != UP:
-            self.head.setheading(DOWN)
+        """Change the snake's direction to down if not currently moving up."""
+        if self.head.heading() != UP:  # If the snake is not currently moving up
+            self.head.setheading(DOWN)  # Change the snake's direction to down
 
     def left(self):
-        if self.head.heading() != RIGHT:
-            self.head.setheading(LEFT)
+        """Change the snake's direction to left if not currently moving right."""
+        if self.head.heading() != RIGHT:  # If the snake is not currently moving right
+            self.head.setheading(LEFT)  # Change the snake's direction to left
 
     def right(self):
-        if self.head.heading() != LEFT:
-            self.head.setheading(RIGHT)
-
+        """Change the snake's direction to right if not currently moving left."""
+        if self.head.heading() != LEFT:  # If the snake is not currently moving left
+            self.head.setheading(RIGHT)  # Change the snake's direction to right
