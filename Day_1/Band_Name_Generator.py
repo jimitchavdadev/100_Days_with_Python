858b0abc937310c92d3d71d9ee317239b7cc09b4
Jimit Chavda <jimitchavdadev@gmail.com>
2024-06-15 11:55:09 +0530

    Day-1

--- Day_1/Band_Name_Generator.py
+++ Day_1/Band_Name_Generator.py
@@ -1,6 +1,13 @@
 # Band Name Generator
 
-print("welcome to the band name Generator.")
-city_name=input("whats the name of the city you grew up in?\n")
-pet_name=input("whats your pet's name?\n")
-print(f"your band name can be {city_name} {pet_name}")
+# Print a welcome message to the user
+print("Welcome to the Band Name Generator.")
+
+# Prompt the user to input the name of the city they grew up in and store the response in the variable city_name
+city_name = input("What's the name of the city you grew up in?\n")
+
+# Prompt the user to input their pet's name and store the response in the variable pet_name
+pet_name = input("What's your pet's name?\n")
+
+# Generate and print the band name by combining the city_name and pet_name
+print(f"Your band name can be {city_name} {pet_name}")
